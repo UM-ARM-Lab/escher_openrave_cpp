6951941d1acbb55f8453536487641cd6dc761071
chenxgu <33813729+chenxgu@users.noreply.github.com>
2019-05-27 04:01:48 -0400

    Update generate_depth_map.py

--- scripts/generate_depth_map.py
+++ scripts/generate_depth_map.py
@@ -1,12 +1,13 @@
-import pickle, IPython, os, math
+import pickle, IPython, os, math, shutil
 import numpy as np
+import matplotlib.pyplot as plt
 
 RESOLUTION = 0.025
 SIDE = 3.0
-GROUND_MAX_DEPTH = 3.0
+GROUND_MAX_DEPTH = 4.0
 RADIUS = 1.5
-WALL_MAX_DEPTH = 1.5
-WALL_MAX_HEIGHT = 3.0
+WALL_MAX_DEPTH = 2.0
+WALL_MAX_HEIGHT = 2.0
 
 
 def rotate_coordinate_system(coordinates, theta):
@@ -35,13 +36,19 @@ def rotate_coordinate_system(coordinates, theta):
 
 def point_inside_polygon(point, vertices):
     """
-    vertices: [vertex1, vertex2, vertex3, vertex4]
+    Inputs:
+    "point" should be a numpy array
+
+    "vertices" should be a list of four numpy arrays which is the coordinates of vertices of a quadrilateral patch.
+
+    Output:
+    If the point is inside of the polygon, return True; otherwise, return False.
     """
     prev_value = None
     num_vertices = len(vertices)
     for i in range(num_vertices):
         begin = vertices[i]
-        end = vertices[(i + 1) % i]
+        end = vertices[(i + 1) % num_vertices]
         vector1 = end - begin
         vector2 = point - begin
         value = vector1[0] * vector2[1] - vector1[1] * vector2[0]
@@ -63,36 +70,48 @@ def patch_depth_map(entire_map, map_type, resolution, vertices):
     "entire_map" should be a 2d numpy array. It will be modified in place.
 
     "map_type" should be either "ground" or "wall".
-    
+
     "resolution" should be a float which represents the distance (in meter) between adjacent pixels on the depth map.
 
-    "vertices" should be the coordinates of vertices of a quadrilateral patch. 
+    "vertices" should be the coordinates of vertices of a quadrilateral patch.
     """
     vertices_array = np.array(vertices)
     normal = np.cross(vertices[0] - vertices[1], vertices[0] - vertices[2])
     if map_type == "ground":
         x_min, x_max = np.min(vertices_array[:, 0]), np.max(vertices_array[:, 0])
         y_min, y_max = np.min(vertices_array[:, 1]), np.max(vertices_array[:, 1])
-        if x_min > SIDE / 2 || x_max < -SIDE / 2 || y_min > side / 2 || y_max < -SIDE / 2:
+        if x_min > SIDE / 2 or x_max < -SIDE / 2 or y_min > SIDE / 2 or y_max < -SIDE / 2:
             return
 
-        for x in range(math.floor(x_min / resolution) * resolution, math.ceil(x_max / resolution) * resolution, resolution):
-            if x > SIDE / 2 || x < -SIDE / 2:
-                return
+        for x_temp in range(int(math.ceil(x_min / resolution)), int(math.ceil(x_max / resolution))):
+            x = x_temp * resolution
+            if x < -SIDE / 2:
+                continue
 
-            for y in range(math.floor(y_min / resolution) * resolution, math.ceil(y_max / resolution) * resolution, resolution):
-                if y > SIDE / 2 || y < -SIDE / 2:
-                    return
+            if x > SIDE / 2:
+                break
+
+            for y_temp in range(int(math.ceil(y_min / resolution)), int(math.ceil(y_max / resolution))):
+                y = y_temp * resolution
+                if y < -SIDE / 2:
+                    continue
+
+                if y > SIDE / 2:
+                    break
 
                 z = vertices[0][2] - ((x - vertices[0][0]) * normal[0] + (y - vertices[0][1]) * normal[1]) / normal[2]
                 z = round(z, 2)
-                if point_inside_polygon(np.array(x, y, z), vertices_array):
-                    idx_1 = int((SIDE / 2 - y) / resolution) 
-                    idx_2 = int((SIDE / 2 - x) / resolution)
-                    if entire_depth_map[idx_1][idx_2] > z:
-                        entire_depth_map[idx_1][idx_2] = z
+                if point_inside_polygon(np.array([x, y, z]), vertices_array):
+                    idx_1 = int(math.ceil((SIDE / 2 - y) / resolution))
+                    idx_2 = int(math.ceil((SIDE / 2 - x) / resolution))
+                    if entire_map[idx_1][idx_2] > WALL_MAX_HEIGHT - z:
+                        entire_map[idx_1][idx_2] = WALL_MAX_HEIGHT - z
+                        print(round(x, 3), round(y, 3), WALL_MAX_HEIGHT - z)
+
+        # IPython.embed()
 
     elif map_type == "wall":
+        pass
 
 
     else:
@@ -110,24 +129,25 @@ def entire_depth_map(coordinates, map_type, resolution=RESOLUTION):
     "map_type" should be either "ground" or "wall".
 
     "resolution" should be a float which represents the distance (in meter) between adjacent pixels on the depth map.
-    
+
     Output:
     a 2d numpy array which represents a depth map.
     """
     if map_type == "ground":
-        entire_map = np.ones((int(SIDE / resolution), int(SIDE / resolution)), dtype=float) * GROUND_MAX_DEPTH
+        entire_map = np.ones((int(SIDE / resolution) + 1, int(SIDE / resolution) + 1), dtype=float) * GROUND_MAX_DEPTH
         for i in range(coordinates.shape[0] // 4):
-            patch_depth_map(entire_map, "ground", resolution, coordinates[4*i], coordinates[4*i+1], coordinates[4*i+2], coordinates[4*i+3])
+            patch_depth_map(entire_map, "ground", resolution, [coordinates[4*i], coordinates[4*i+1], coordinates[4*i+2], coordinates[4*i+3]])
+        IPython.embed()
 
     elif map_type == "wall":
-        entire_map = np.ones((int(WALL_MAX_HEIGHT / resolution), int(2 * np.pi * RADIUS / resolution)), dtype=float) * WALL_MAX_DEPTH
+        entire_map = np.ones((int(WALL_MAX_HEIGHT / resolution) + 1, int(2 * np.pi * RADIUS / resolution) + 1), dtype=float) * WALL_MAX_DEPTH
         for i in range(coordinates.shape[0] // 4):
-            patch_depth_map(entire_map, "wall", resolution, coordinates[4*i], coordinates[4*i+1], coordinates[4*i+2], coordinates[4*i+3])
+            patch_depth_map(entire_map, "wall", resolution, [coordinates[4*i], coordinates[4*i+1], coordinates[4*i+2], coordinates[4*i+3]])
 
     else:
         print('wrong type')
         exit(1)
-    
+
     return entire_map
 
 
@@ -157,11 +177,11 @@ def main():
             assert(pose[0] == 0.0 and pose[1] == 0.0)
             ground_patch_coordinates = rotate_coordinate_system(environments[environment_index]['ground'], pose[2])
             ground_depth_map = entire_depth_map(ground_patch_coordinates, 'ground', RESOLUTION)
-            file = ('../data/minimal/ground_depth_maps/' + str(example_id), 'w')
+            file = open('../data/minimal/ground_depth_maps/' + str(example_id), 'w')
             pickle.dump(ground_depth_map, file)
             wall_patch_coordinates = rotate_coordinate_system(environments[environment_index]['others'], pose[2])
             wall_depth_map = entire_depth_map(wall_patch_coordinates, 'wall', RESOLUTION)
-            file = ('../data/minimal/wall_depth_maps/' + str(example_id), 'w')
+            file = open('../data/minimal/wall_depth_maps/' + str(example_id), 'w')
             pickle.dump(wall_depth_map, file)
             final_status_list.append(pose[3:6])
             minimal_ddyn_list.append(min(pose_to_ddyn[pose]))
