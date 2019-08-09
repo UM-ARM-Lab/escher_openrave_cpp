9665ed5cd6478e15b9d4e0aa8d194c93fbc3980b
chenxgu <chenxgu@umich.edu>
2019-08-09 11:31:00 -0400

    n

--- scripts/neural_network/p1p2/model_B_1.py
+++ scripts/neural_network/p1p2/model_B_1.py
@@ -13,57 +13,41 @@ class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
         self.ground_net = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=1, out_channels=64, kernel_size=5, padding=1),
             nn.LeakyReLU(),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=5, padding=1),
             nn.LeakyReLU(),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
-            nn.LeakyReLU(),
-            nn.MaxPool2d(kernel_size=2),
-
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
-            nn.LeakyReLU(),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
-            nn.LeakyReLU(),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=5, padding=1),
             nn.LeakyReLU(),
             nn.MaxPool2d(kernel_size=2),
 
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=5, padding=1),
             nn.LeakyReLU(),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=5, padding=1),
             nn.LeakyReLU(),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=5, padding=1),
             nn.LeakyReLU(),
             nn.MaxPool2d(kernel_size=2),
+
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=5, padding=1),
+            nn.LeakyReLU(),
 
             Flatten(),
         )
 
         self.wall_net = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=1, out_channels=64, kernel_size=5, padding=1),
             nn.LeakyReLU(),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=5, padding=1),
             nn.LeakyReLU(),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
-            nn.LeakyReLU(),
-            nn.MaxPool2d(kernel_size=2),
-
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
-            nn.LeakyReLU(),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
-            nn.LeakyReLU(),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=5, padding=1),
             nn.LeakyReLU(),
             nn.MaxPool2d(kernel_size=2),
 
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=5, padding=1),
             nn.LeakyReLU(),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=5, padding=1),
             nn.LeakyReLU(),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
-            nn.LeakyReLU(),
-            nn.MaxPool2d(kernel_size=2),
 
             Flatten(),
         )
@@ -73,7 +57,7 @@ class Model(nn.Module):
             nn.LeakyReLU(),
             nn.Linear(64, 64),
             nn.LeakyReLU(),
-            nn.Linear(64, 8*8*5),
+            nn.Linear(64, 9*9*5),
             nn.LeakyReLU(),
         )
 
@@ -82,7 +66,7 @@ class Model(nn.Module):
             nn.LeakyReLU(),
             nn.Linear(64, 64),
             nn.LeakyReLU(),
-            nn.Linear(64, 5*31*5),
+            nn.Linear(64, 5*119*5),
             nn.LeakyReLU(),
         )
 
@@ -103,16 +87,16 @@ class Model(nn.Module):
     def forward(self, ground_depth_map, wall_depth_map, p2):
         """
         "ground_depth_map" is a 2d array of shape (1, 65, 65)
-        "wall_depth_map" is a 2d array of shape (1, 41, 252)
+        "wall_depth_map" is a 2d array of shape (1, 25, 252)
         "p2" is a 1d array of shape (3,)
         """
         arr1 = self.ground_net(ground_depth_map)
-        arr1 = arr1.view(-1, 64, 8*8)
-        p2_ground = self.p2_net_ground(p2).view(-1, 8*8, 5)
+        arr1 = arr1.view(-1, 64, 9*9)
+        p2_ground = self.p2_net_ground(p2).view(-1, 9*9, 5)
         arr1 = torch.bmm(arr1, p2_ground).view(-1, 64*5)
         arr2 = self.wall_net(wall_depth_map)
-        arr2 = arr2.view(-1, 64, 5*31)
-        p2_wall = self.p2_net_wall(p2).view(-1, 5*31, 5)
+        arr2 = arr2.view(-1, 64, 5*119)
+        p2_wall = self.p2_net_wall(p2).view(-1, 5*119, 5)
         arr2 = torch.bmm(arr2, p2_wall).view(-1, 64*5)
         arr = torch.cat((arr1, arr2), 1)
         z = self.fc(arr)
